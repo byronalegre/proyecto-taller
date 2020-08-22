9837862ce05843f0541898a2e4c26566a46e5292
Byron <68613926+byronalegre@users.noreply.github.com>
2020-08-22 18:38:09 -0300

    Add files via upload

--- js/admin.js
+++ js/admin.js
@@ -1,25 +1,22 @@
 var base = location.protocol+'//'+location.host;
 var route = document.getElementsByName('routeName')[0].getAttribute('content');
-
-document.addEventListener('DOMContentLoaded', function(){
-	
+//------------------------------------------------------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function(){	
 	document.getElementsByClassName('lk-'+route)[0].classList.add('active');
-
 
 	var btn_deleted = document.getElementsByClassName('btn-deleted');
 
 		for(i=0; i < btn_deleted.length; i++){
 			btn_deleted[i].addEventListener('click', delete_object);
 		}
-
 });
-
+//------------------------------------------------------------------------------------------------------------------------------
+if(route == "piezas_editar" || route == "piezas_agregar"){
 $(document).ready(function(){
 	editor_init('editor');
 });
-
+//------------------------------------------------------------------------------------------------------------------------------
 function editor_init(field){
-
 	CKEDITOR.replace(field,{
 		toolbar: [
 		{ name: 'clipboard', items: ['Cut','Copy','Paste','PasteText','-','Undo','Redo'] } ,
@@ -29,11 +26,10 @@ function editor_init(field){
 		]
 	});
 }
+}
 
-
-
+//------------------------------------------------------------------------------------------------------------------------------
 function delete_object(e){
-
 	e.preventDefault();
 	var object = this.getAttribute('data-object');
 	var action = this.getAttribute('data-action');
@@ -49,7 +45,6 @@ function delete_object(e){
 		title = "Seguro que desea restaurar el elemento?";
 		text = "Una vez restaurado, volverá a estar disponible.";
 	}
-
 	swal({
 	  title: title,
 	  text: text,
@@ -60,10 +55,109 @@ function delete_object(e){
 	  if (willDelete) {
 	  	window.location.href = url;	   
 	  }
-	});
-	
+	});	
 }
 
+//------------------------------------------------------------------------------------------------------------------------------
+//FUNCION PARA CREAR TABLA AGREGAR PRODUCTOS A COMPRA
+var count = 0;
+var acum = 0;
+var list = {
+			 'datos' :[]
+			};
+$(function(){	
+	//agrega fila a tabla compra
+	$('#agregar').click(function(e){
+		e.preventDefault();		
+		var combo = document.getElementById("producto");
+		var selected = combo.options[combo.selectedIndex].text;//nombre 
+		var cant = document.getElementById("cantidad").value;
+ 		var price = document.getElementById("precio").value;	
+ 		var id_prod = combo.options[combo.selectedIndex].value;
 
+ 		if(cant>0 && price>0){//esto valida que no se ingresen valores 0
+		var tr = '<tr><td hidden="true">'+count+'</td><td>'+selected+'</td><td>'+cant+'</td><td>'+price+'</td><td>'+cant*price+'</td><td><input type="button" class="borrar btn btn-danger btn-sm" value="Eliminar"></td></tr>';
+		
+		$('tbody').append(tr); 
+	    list.datos.push({
+	    "id":count,
+	    "id_p": id_prod,
+	    "producto": selected,
+	    "cantidad": cant,
+	    "precio": price
+	  	});
+		
+		var json = JSON.stringify(list.datos); // aqui tienes la lista de objetos en Json
+	//	var Obj = JSON.parse(json); //Parsea el Json al objeto anterior.
+		count++;
 
+		$("#productos").val(json);	
+      
+//ACTUALIZA Y ACUMULA TOTAL
+		for (var item in list.datos){
+			var td = parseInt(list.datos[item].precio * list.datos[item].cantidad);
+		}
+		acum += td;
+		
+		$('h6').html("Total: $"+ acum);
+		}
+	})		
 
+//BORRA FILA, ACTUALIZA ARRAY DE PRODUCTOS y TOTAL $
+	$('body').on('click', 'input.borrar', function(e) {		
+	   e.preventDefault();
+	   var index = $(this).closest("tr").index();
+       var id = $(this).parents("tr").find("td")[0].innerHTML; 
+       var erase = $(this).parents("tr").find("td")[4].innerHTML;
+
+       for(var item in list.datos){     	
+        	if(list.datos[item].id == id){
+       			list.datos.splice(index,1);
+       		}
+       }
+
+		if(list.datos.length == 0){			
+			location.reload();
+		}
+		
+        var json = JSON.stringify(list.datos);
+		$("#productos").val(json);
+   		$(this).parents('tr').remove(); 
+
+   		acum -= erase;
+   		$('h6').html("Total: $"+ acum);
+   	});    
+})
+
+//------------------------------------------------------------------------------------------------------------------------------
+//FUNCION CALCULA TOTAL PANEL INICIO
+
+var acum_tot_compras = 0;
+$(document).ready(function(){
+	var json_c = document.getElementById('all_prods').value;
+	var Obj_c = JSON.parse(json_c);
+	for(var item in Obj_c){
+		var aux=Obj_c[item];
+		var prods = JSON.parse(aux.productos)
+		for(var i in prods){
+		var aux2=prods[i];
+	//	console.log(parseInt(aux2.cantidad*aux2.precio))
+		acum_tot_compras += parseInt(aux2.cantidad*aux2.precio);
+		}
+	}	
+	$('h5').append(acum_tot_compras);
+});
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+/*var acumular = 0;
+$(function(){
+	var json_home = $('#total_home').val();
+	var Obj_home = JSON.parse(json_home);
+	console.log(Obj_home)
+	for(var items in Obj_home){
+		var aux=Obj_home[items];		
+	//	console.log(aux.producto)
+		acumular+=parseInt(aux.cantidad * aux.precio);
+	}	
+	$('h6').append(acumular);
+});*/
